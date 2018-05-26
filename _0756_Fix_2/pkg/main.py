399eeb9a1bcd1528473ebfea53439a5095a5115b
jason.gastelum <jason.gastelum@gmail.com>
2018-05-25 19:32:15 -0600

    Learned about using a single directory as a program and package.

---
+++ _0756_Fix_2/pkg/main.py
@@ -0,0 +1,3 @@
+import sys
+#[print(p) for p in sys.path]
+import spam
---
+++ _0756_Fix_2/pkg/spam.py
@@ -0,0 +1,3 @@
+# This full path absolute import is relative to 'C:\_Gastelum\Development\Learning_Python_5th_Edition'
+# which is on sys.path because I manually set PYTHONPATH.
+import _0756_Fix_2.pkg.eggs
